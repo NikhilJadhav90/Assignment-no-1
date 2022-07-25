165f631ac999a5d7294dc510424918539269fd50
Nikhil Jadhav <91952621+divinesssssssssssssssssssssss@users.noreply.github.com>
2022-07-25 13:50:25 +0530

    Program to Find Factorial

---
+++ factorial.java
@@ -0,0 +1,11 @@
+/*
+Write a program to find the factorial value of any number entered through 
+the keyboard.
+ */
+public class factorial {
+    public static void main(String args[])
+    {
+        
+    }
+    
+}
