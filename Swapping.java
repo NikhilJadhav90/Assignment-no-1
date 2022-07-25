61fe11a474f8cce3033b4677a1e32804ad5ec587
Nikhil Jadhav <91952621+divinesssssssssssssssssssssss@users.noreply.github.com>
2022-07-25 13:46:16 +0530

    Swap Two Numbers

---
+++ Swapping.java
@@ -0,0 +1,15 @@
+/*
+Program to Swap Two Numbers
+ */
+public class Swapping {
+    public static void main(String args[])
+    {
+       int x=4,y=5,t;
+       
+       t = x;  
+       x = y;  
+       y = t;  
+       System.out.println("After swapping: X= "+x +"  Y= " + y);  
+       System.out.println("");  
+    }
+}
